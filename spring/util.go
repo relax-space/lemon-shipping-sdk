936f35fcf5cf536eedc016ce8fccb6f25fed6567
xiao.xinmiao <xiaoxm_001@163.com>
2018-02-10 16:59:05 +0800

    added error code

--- spring/util.go
+++ spring/util.go
@@ -15,3 +15,10 @@ func toBase64(dto interface{}) (data string, err error) {
 	data = base64.StdEncoding.EncodeToString([]byte(encodeData))
 	return
 }
+
+const (
+	SUC = "SUC" //success
+	E01 = "E01" //system error,can re-try
+	E02 = "E02" //bad request format or response format
+	E03 = "E03" //message from shipping
+)
